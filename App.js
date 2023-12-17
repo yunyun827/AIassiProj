caa768200b89b416adbf160124ae6e951952c968
yunyun827 <imyure@gmail.com>
2023-12-17 23:17:04 +0900

    basic folders added

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react' 
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+
+  )
+
+}
