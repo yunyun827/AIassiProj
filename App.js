3091353074319637f3405e8c1a8bb5892a696c69
yunyun827 <imyure@gmail.com>
2023-12-17 23:31:48 +0900

    navigation working

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react' 
+import AppNavigation from './src/navigation'
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>App</Text>
-    </SafeAreaView>
+   <AppNavigation />
 
   )
 
